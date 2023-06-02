2779d17b3694a6151e27b517ac093e2259c8ead3
jazzpujols34 <ro5112@hotmail.com>
2023-06-02 14:22:42 +0800

    add UPDATE to app.py

--- app.py
+++ app.py
@@ -5,9 +5,60 @@ from flask import Flask, render_template, request
 from pymongo import MongoClient
 from pymongo.collection import Collection
 from pymongo.database import Database
+# access your MongoDB Atlas cluster
+load_dotenv()
+connection_string: str = os.environ.get("CONNECTION_STRING")
+mongo_client: MongoClient = MongoClient(connection_string)
 
+# add in your database and collection from Atlas
+database: Database = mongo_client.get_database("bookshelf")
+collection: Collection = database.get_collection("books")
+
+# For testing purpose, verify book can be added to collection
+# book = {"title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "year": 1925}
+# collection.insert_one(book)
+
+# instantiating new object with "name"
 app: Flask = Flask(__name__)
+
 #our initial form page
 @app.route('/')
 def index():
-    return "Hi!"
+    return render_template("index.html")
+
+# CREATE and READ
+@app.route('/books', methods=["GET","POST"])
+def books():
+    if request.method == 'POST':
+        # CREATE
+        book: str = request.json['book']
+        pages: str = request.json['pages']
+    
+        # insert new book into books collection in MongoDB
+        collection.insert_one({"book":book, "pages":pages})
+    
+        return f"CREATE: Your book {book} ({pages} pages) has been added to your bookshelf.\n"
+
+    elif request.method == 'GET':
+        # READ
+        bookshelf = list(collection.find())
+        novels = []
+    
+        for titles in bookshelf:
+            book = titles['book']
+            pages = titles['pages']
+            shelf = {'book':book, 'pages':pages}
+            novels.insert(0,shelf)
+
+    return novels
+
+# UPDATE
+@app.route("/books/<string:book_id>", methods = ['PUT'])
+def update_book(book_id: str):
+    new_book: str = request.json['book']
+    new_pages: str = request.json['pages']
+    collection.update_one({"_id": bson.ObjectId(book_id)}, {"$set": {"book": new_book, "pages": new_pages}})
+    
+    return f"UPDATE: Your book has been updated to: {new_book} ({new_pages} pages).\n"
+
+
